45e29fd1b001a7afb311f940a9b5cffb9baca380
cakejune <tyq444@gmail.com>
2022-12-15 16:11:39 -0500

    commit tree b4a4f150cf86cf405afff409963708d9db3e3cd2

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,15 @@
 class Author < ApplicationRecord
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :phone_number, length: { is: 10 }
+
+
+
 end
+
+
+
+# unless email.match?(/flatironschool.com/)
+#     errors.add(:email, "We're only allowed to have people who work for the company in the database!")
+#   end
